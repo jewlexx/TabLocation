c0f4597e1268d216076b9fa9f1d5bd66e8a57fa4
James Grasso <grassojames5@gmail.com>
2020-11-29 15:09:27 +1100

    Fixed major bug

--- src/main/java/com/jamesafk/tablocation/TabLocation.java
+++ src/main/java/com/jamesafk/tablocation/TabLocation.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -15,7 +14,7 @@ public final class TabLocation extends JavaPlugin implements Listener {
 
     public static Logger log = Bukkit.getLogger();
     public static String ver;
-    public static Permission hide = new Permission("tablocation.hide");
+    public static String hide = "tablocation.hide";
     public static String javaver = System.getProperty("java.version");
     public static FileConfiguration config;
     public static boolean enviroment = true;
@@ -26,12 +25,6 @@ public final class TabLocation extends JavaPlugin implements Listener {
         // Plugin startup logic
 
         ver = this.getDescription().getVersion();
-
-        /*
-        if (Bukkit.getPluginManager().getPermission("tablocation.hide") == null) {
-            Bukkit.getPluginManager().addPermission(hide);
-        }
-         */
 
         Bukkit.getPluginManager().registerEvents(this, this);
 
