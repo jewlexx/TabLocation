ff272ae70cbc8e40ac4cb32c7bc2cb2c0613511f
Juliette Cordor <me@cordor.dev>
2025-02-23 19:49:52 +1100

    Fixed colours

--- src/main/java/com/jamesafk/tablocation/TabLocation.java
+++ src/main/java/com/jamesafk/tablocation/TabLocation.java
@@ -78,7 +78,7 @@ public final class TabLocation extends JavaPlugin implements Listener {
             if (world.equalsIgnoreCase("normal")) {
                 world = "Overworld";
             }
-            world = ", The " + world;
+            world = ", §5 The " + world + "§f";
 
             if (!enviroment) {
                 world = "";
