e06bc2b5e41385e194e66de3981d996058b74f67
Juliette Cordor <me@cordor.dev>
2026-08-25 09:24:44 +1000

    added old java code

---
+++ src/main/java/dev/cordor/tablocation/UpdateChecker.java
@@ -0,0 +1,48 @@
+package dev.cordor.tablocation;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.jewelexx.tablocation.github.Converter;
+import com.jewelexx.tablocation.github.Tags;
+
+public class UpdateChecker {
+    private JavaPlugin plugin;
+
+    public UpdateChecker(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    static String readStringFromURL(String requestURL) throws IOException, URISyntaxException {
+        try (Scanner scanner = new Scanner(new URI(requestURL).toURL().openStream(),
+                StandardCharsets.UTF_8.toString())) {
+            scanner.useDelimiter("\\A");
+            return scanner.hasNext() ? scanner.next() : "";
+        }
+    }
+
+    public void getVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try {
+                String inputStream = readStringFromURL(
+                        "https://api.github.com/repos/jewlexx/TabLocation/tags");
+
+                Tags latestTag = Converter.fromJsonString(inputStream)[0];
+                String name = latestTag.getName();
+
+                consumer.accept(name);
+            } catch (IOException exception) {
+                this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
+            } catch (URISyntaxException exception) {
+                this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
+            }
+        });
+    }
+}
