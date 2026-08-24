89ec57846adff1e0b0e403b8ac69d1cc96a08042
Juliette Cordor <me@cordor.dev>
2026-08-25 08:55:56 +1000

    added kotlin project files

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "2.4.10"
+    id("com.gradleup.shadow") version "9.6.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:26.1.1-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/kotlin/dev/cordor/tablocation/Tablocation.kt
@@ -0,0 +1,14 @@
+package dev.cordor.tablocation
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Tablocation : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
